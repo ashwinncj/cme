ca8740639c7ca322946c1c0bbb794f0cae457bb3
Darshan Ail <44797504+darsh1076@users.noreply.github.com>
2018-12-13 12:38:22 +0530

    Update footer.php
    Jivo Chat Added

--- ci/application/views/templates/footer.php
+++ ci/application/views/templates/footer.php
@@ -117,10 +117,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     @media (max-width: 478px){
         .footer-clean .item.social {
             text-align: center;
-            
+
         }
     }
 </style>
+
+<!--JIVO CHAT CODE SNIPPET --- BEGIN JIVOSITE CODE {literal} -->
+    <script type='text/javascript'>
+        (function(){ var widget_id = 'yXoFtysEmr'; var d = document; var w = window; function l(){var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/' + widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss); }if (d.readyState == 'complete'){l(); } else{if (w.attachEvent){w.attachEvent('onload', l); } else{w.addEventListener('load', l, false); }}})();
+    </script>
+<!-- {/literal} END JIVOSITE CODE -->
+
 <div class="footer-clean" style="padding-bottom:0px;">
     <footer>
         <div class="container">
