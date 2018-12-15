ec0d70c91a7d03b4970057e7b930146ef51a656a
ashwinncj <30401526+ashwinncj@users.noreply.github.com>
2018-12-15 13:24:36 +0530

    Update admin.php

--- ci/application/views/admin.php
+++ ci/application/views/admin.php
@@ -45,21 +45,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 </tr>
             </thead>
             <tbody>
-                <?php 
-                $count =0;
-                foreach ($info as $value){
+                <?php
+                $count = 0;
+                foreach ($info as $value) {
                     $count++;
                     ?>
-                <tr>
-                    <td><?php echo $count;?></td>
-                    <td><a href="<?php echo base_url('admin/company/'.$value['uid']); ?>" style="color: #047dff"><?php echo $value['company_name'];?></a></td>
-                    <td><?php echo $value['location'];?></td>
-                    <td><?php echo $value['contact_person'];?></td>
-                    <td><?php echo $value['email'];?></td>
-                    <td><?php echo $value['phone_number'];?></td>
-                    <td><?php echo $value['category'];?></td>
-                    <td><?php echo $value['webpage'];?></td>
-                </tr>
+                    <tr>
+                        <td><?php echo $count; ?></td>
+                        <td><a href="<?php echo base_url('admin/company/' . $value['uid']); ?>" style="color: #047dff"><?php echo $value['company_name']; ?></a></td>
+                        <td><?php echo isset($value['location']) ? $value['location'] : ''; ?></td>
+                        <td><?php echo isset($value['contact_person']) ? $value['contact_person'] : ''; ?></td>
+                        <td><?php echo isset($value['email']) ? $value['email'] : ''; ?></td>
+                        <td><?php echo isset($value['phone_number']) ? $value['phone_number'] : ''; ?></td>
+                        <td><?php echo isset($value['category']) ? $value['category'] : ''; ?></td>
+                        <td><?php echo isset($value['webpage']) ? $value['webpage'] : ''; ?></td>
+                    </tr>
                 <?php } ?>
             </tbody>
         </table>
