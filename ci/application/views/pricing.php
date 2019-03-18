02e93375febb32641e9029ca57b2979c68bb5c1e
ashwinncj <30401526+ashwinncj@users.noreply.github.com>
2019-03-18 16:22:02 +0530

    Pricing Updates

--- ci/application/views/pricing.php
+++ ci/application/views/pricing.php
@@ -6,11 +6,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <head>
         <title>ConstructMe - Pricing</title>
         <?php $this->load->view('templates/header'); ?>
+        <style>
+            .pricing-img{
+                max-width:100%; 
+                max-height: 60vh;
+                box-shadow: 1px 1px 2px 0px black;
+            }
+            .pricing-img:hover{
+                box-shadow: 2px 1px 8px 0px black;
+                cursor: pointer;
+            }
+        </style>
     </head>
     <body style="overflow-x: hidden">
-        <?php $this->load->view('templates/navigation');?>
-        <div>
-            <img src="<?php echo base_url(); ?>assets/img/under-construction.jpg" style="height:600px;width:100%">
+        <?php $this->load->view('templates/navigation'); ?>
+        <div class="col-md-12 row" style="padding: 20px">
+            <div class="col-md-4 text-center offset-2">
+                <a href="<?php echo base_url('signup'); ?>">
+                    <img class="pricing-img" src="<?php echo base_url(); ?>assets/img/free-reg.png">
+                </a>
+            </div>
+            <div class="col-md-4 text-center">
+                <img class="pricing-img" src="<?php echo base_url(); ?>assets/img/paid-reg.png" style="cursor: not-allowed">
+            </div>
         </div>
         <?php $this->load->view('templates/footer'); ?>
 
